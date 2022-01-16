f6719dbeb80a7569c7937603b74b92801e1df3f2
Stéphane Bressani <stephane@bressani.dev>
2022-01-16 18:26:42 +0100

    Initial Commit

---
+++ astrology_macos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  astrology_macos
+//
+//  Created by Stéphane on 16.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ astrology_macos/astrology_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  astrology_macosApp.swift
+//  astrology_macos
+//
+//  Created by Stéphane on 16.01.22.
+//
+
+import SwiftUI
+
+@main
+struct astrology_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
